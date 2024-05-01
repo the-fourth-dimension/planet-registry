12c57a5de65caea98c5e1508bf78abe161888e3d
Arth <probablyarth@gmail.com>
2024-05-02 02:37:54 +0530

    Context: rename _db to db

--- pkg/repositories/main.go
+++ pkg/repositories/main.go
@@ -10,7 +10,7 @@ type Context struct {
 	PlanetRepository     *PlanetRepository
 	InviteCodeRepository *InviteCodeRepository
 	ConfigRepository     *ConfigRepository
-	_db                  *gorm.DB
+	db                   *gorm.DB
 }
 
 func NewContext(db *gorm.DB) *Context {
@@ -18,14 +18,14 @@ func NewContext(db *gorm.DB) *Context {
 		PlanetRepository:     NewPlanetRepository(db),
 		InviteCodeRepository: NewInviteCodeRepository(db),
 		ConfigRepository:     NewConfigRepository(db),
-		_db:                  db,
+		db:                   db,
 	}
 }
 
 func (ctx *Context) ExecuteTransaction(transaction func(*gorm.DB) bool) bool {
-	err := ctx._db.Transaction(
+	err := ctx.db.Transaction(
 		func(tx *gorm.DB) error {
-			if transaction(ctx._db) {
+			if transaction(ctx.db) {
 				return nil
 			}
 			return errors.New("Transaction Failed")
