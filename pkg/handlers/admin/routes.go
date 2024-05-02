8fecfa89b896206803844b3981dcbc42da74432c
Arth <probablyarth@gmail.com>
2024-05-02 13:27:56 +0530

    admin: put /id route

--- pkg/handlers/admin/routes.go
+++ pkg/handlers/admin/routes.go
@@ -3,6 +3,7 @@ package AdminHandler
 import (
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
@@ -16,6 +17,58 @@ func (h *adminHandler) get(ctx *gin.Context) {
 		ctx.AbortWithError(http.StatusInternalServerError, findResult.Error)
 	}
 	ctx.JSON(http.StatusOK, findResult)
+}
+
+type putRequest struct {
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+func (h *adminHandler) putById(ctx *gin.Context) {
+	id := ctx.Param("id")
+	if id == "" {
+		ctx.Error(HttpError.NewHttpError("Missing param fields", "id", http.StatusBadRequest))
+		return
+	}
+	uintId, error := strconv.ParseUint(id, 10, 64)
+	if error != nil {
+		ctx.Error(HttpError.NewHttpError("Invalid field", "id", http.StatusBadRequest))
+	}
+	findResult := h.ctx.AdminRepository.FindFirst(&models.Admin{Model: gorm.Model{ID: uint(uintId)}})
+	if findResult.Error != nil {
+		if errors.Is(findResult.Error, gorm.ErrRecordNotFound) {
+			ctx.Error(HttpError.NewHttpError("Admin.ID Not found", id, http.StatusNotFound))
+			return
+		}
+		ctx.AbortWithError(http.StatusInternalServerError, findResult.Error)
+		return
+	}
+	var input putRequest
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.Error(HttpError.NewHttpError("invalid input", err.Error(), http.StatusBadRequest))
+		return
+	}
+	changed := false
+	if input.Password != "" {
+		findResult.Result.Password = input.Password
+		changed = true
+	}
+	if input.Username != "" {
+		findResult.Result.Username = input.Username
+		changed = true
+	}
+	if changed {
+		saveResult := h.ctx.AdminRepository.Save(&findResult.Result)
+		if saveResult.Error != nil {
+			ctx.AbortWithError(http.StatusInternalServerError, saveResult.Error)
+			return
+		}
+	}
+	if !changed {
+		ctx.JSON(http.StatusNoContent, gin.H{"message": "Nothing to change"})
+		return
+	}
+	ctx.Status(http.StatusAccepted)
 }
 
 func (h *adminHandler) post(ctx *gin.Context) {
