65efa39059a03d437c20e25d459618415a7575cf
Arth <probablyarth@gmail.com>
2024-05-04 20:11:24 +0530

    push trimmed code to db

--- pkg/models/Invite.go
+++ pkg/models/Invite.go
@@ -12,10 +12,11 @@ type Invite struct {
 	Code string `gorm:"size:255;not null;unique" json:"code"`
 }
 
-func (i *Invite) BeforeSave(tx *gorm.DB) (err error) {
+func (i *Invite) BeforeSave(scope *gorm.Scope) (err error) {
 	code := strings.Trim(i.Code, " ")
 	if len(code) < 4 {
 		err = errors.New("code-length-too-short")
 	}
+	scope.SetColumn("code", code)
 	return
 }
