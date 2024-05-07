99239f6fbf48530b07a8d3f405d279826e594781
Arth <probablyarth@gmail.com>
2024-05-08 01:06:56 +0530

    tests_invites-handler: testcase 1

--- tests/handlers_invites_test.go
+++ tests/handlers_invites_test.go
@@ -2,17 +2,45 @@ package tests
 
 import (
 	"bytes"
+	"encoding/json"
+	"io"
 	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/jinzhu/gorm"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"github.com/the_fourth_dimension/planet_registry/pkg/database"
 	"github.com/the_fourth_dimension/planet_registry/pkg/env"
+	"github.com/the_fourth_dimension/planet_registry/pkg/lib"
 	"github.com/the_fourth_dimension/planet_registry/pkg/models"
 	"github.com/the_fourth_dimension/planet_registry/pkg/repositories"
 	"github.com/the_fourth_dimension/planet_registry/pkg/routes"
 )
+
+func (suite *InvitesHandlersTestSuite) TestInvitesGetWithNoCodeQuery() {
+	w := httptest.NewRecorder()
+	jsonFile, _ := os.Open("./mock_data/invites.json")
+	byteValue, _ := io.ReadAll(jsonFile)
+	var invites []models.Invite
+	json.Unmarshal(byteValue, &invites)
+	for _, invite := range invites {
+		suite.ctx.InviteRepository.Save(&invite)
+	}
+	token, _ := lib.SignJwt(jwt.MapClaims{"role": 0})
+	req, _ := http.NewRequest("GET", "/invites/", nil)
+	req.Header.Set("Authorization", lib.MakeAuthHeader(token))
+	suite.router.Engine.ServeHTTP(w, req)
+	bodyBytes, _ := io.ReadAll(w.Result().Body)
+	var receivedInvites []models.Invite
+	json.Unmarshal(bodyBytes, &receivedInvites)
+	assert.Equal(suite.T(), 200, w.Code)
+	assert.Equal(suite.T(), len(invites), len(receivedInvites))
+}
 
 func (suite *InvitesHandlersTestSuite) SetupTest() {
 	suite.db.MigrateModels()
