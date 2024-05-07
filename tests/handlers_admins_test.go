1ff38e7bd52b90a9ca05bf3b4d8f24c38f0cc161
Arth <probablyarth@gmail.com>
2024-05-08 00:54:32 +0530

    tests_admins-handler: testcase 11

--- tests/handlers_admins_test.go
+++ tests/handlers_admins_test.go
@@ -152,6 +152,17 @@ func (suite *AdminsHandlersTestSuite) TestAdminsDeleteIdWithNoIdParam() {
 	assert.Equal(suite.T(), 404, w.Code)
 }
 
+func (suite *AdminsHandlersTestSuite) TestAdminsDeleteIdWithIdParamOfNonIntegerDatatype() {
+	w := httptest.NewRecorder()
+
+	token, _ := lib.SignJwt(jwt.MapClaims{"role": 0})
+	req, _ := http.NewRequest("DELETE", "/admins/arth", nil)
+	req.Header.Set("Authorization", lib.MakeAuthHeader(token))
+	suite.router.Engine.ServeHTTP(w, req)
+
+	assert.Equal(suite.T(), 400, w.Code)
+}
+
 func (suite *AdminsHandlersTestSuite) SetupTest() {
 	suite.db.MigrateModels()
 	suite.db.PopulateConfig()
